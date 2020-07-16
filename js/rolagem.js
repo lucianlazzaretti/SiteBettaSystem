fdf2479edb442176e422186b0ea0fc6168cbe7f6
Lucian <Lucian@DESKTOP-4UT1CGB>
2020-07-16 10:56:46 -0300

    primeiro commit

---
+++ js/rolagem.js
@@ -0,0 +1,21 @@
+
+// Quando o usuário rolar a página, execute myFunction
+window.onscroll = function () { rolagem() };
+
+// Obtenha a barra de navegação
+var navbar = document.getElementById("navbar");
+
+// Obter a posição de deslocamento da barra de navegação
+var sticky = navbar.offsetTop;
+
+// Adicione a classe adesiva à barra de navegação quando atingir sua posição de rolagem. Remova "sticky" quando sair da posição de rolagem
+function rolagem() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
+
+
